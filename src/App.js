28a1d5477d91ac9668ae7dadb85b108be7bc7bbb
anano303 <124179773+anano303@users.noreply.github.com>
2024-09-15 21:58:40 +0400

    mobNavbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Layout from './Layouts/Layout.js'
-import Home from './Pages/Home/Home.jsx';
-import About from './Pages/About/About.jsx';
-import Contact from './Pages/Contact/Contact.jsx';
-import { ThemeContext } from './Hooks/ThemeContext.js'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Layout from "./Layouts/Layout.js";
+import Home from "./Pages/Home/Home.jsx";
+import About from "./Pages/About/About.jsx";
+import Contact from "./Pages/Contact/Contact.jsx";
+import { ThemeContext } from "./Hooks/ThemeContext.js";
 import { LanguageContext } from "../src/Hooks/LanguageContext";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Service from './Pages/Service/Service.jsx';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Service from "./Pages/Service/Service.jsx";
+import Prices from "./Pages/Prices/Prices.jsx";
 
 function App() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
   const [language, setLanguage] = useState("ge");
 
   useEffect(() => {
-    const appEl = document.getElementById('app');
+    const appEl = document.getElementById("app");
     if (appEl) {
       appEl.className = theme;
     }
@@ -25,49 +25,57 @@ function App() {
   }, [language]);
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+    setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   };
 
   return (
     <Router>
       <div className="App" id="app">
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
-        <LanguageContext.Provider value={{ language, setLanguage }}>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <Layout>
-                  <Home />
-                </Layout>
-              }
-            />
-            <Route
-              path="/service"
-              element={
-                <Layout>
-                  <Service />
-                </Layout>
-              }
-            />
-            <Route
-              path="/about"
-              element={
-                <Layout>
-                  <About/>
-                </Layout>
-              }
-            />
-        
-            <Route
-              path="/contact"
-              element={
-                <Layout>
-                  <Contact />
-                </Layout>
-              }
-            />
-          </Routes>
+          <LanguageContext.Provider value={{ language, setLanguage }}>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Layout>
+                    <Home />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/service"
+                element={
+                  <Layout>
+                    <Service />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/prices"
+                element={
+                  <Layout>
+                    <Prices />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/about"
+                element={
+                  <Layout>
+                    <About />
+                  </Layout>
+                }
+              />
+
+              <Route
+                path="/contact"
+                element={
+                  <Layout>
+                    <Contact />
+                  </Layout>
+                }
+              />
+            </Routes>
           </LanguageContext.Provider>
         </ThemeContext.Provider>
       </div>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,19 @@ import { TEXTS } from "../../Hooks/Languages";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import BurgerMenu from "./Icons/burger menu icon.png";
+import x from "./Icons/x (2).png";
+import homeIcon from "./Icons/Home.png";
+import sercvIcon from "./Icons/draw_abstract.png";
+import priceIcon from "./Icons/sell.png";
+import aboutIcon from "./Icons/groups_2.png";
+import contactIcon from "./Icons/call.png";
+import xLight from "./Icons/x light.png";
+import homeIconLight from "./Icons/Home (1).png";
+import sercvIconLight from "./Icons/draw_abstract (1).png";
+import priceIconLight from "./Icons/sell (1).png";
+import aboutIconLight from "./Icons/groups_2 (1).png";
+import contactIconLight from "./Icons/call (1).png";
+import mobImage from "./Icons/primery bs logoai 1.png";
 
 const Navbar = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
@@ -14,6 +27,9 @@ const Navbar = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
   return (
     <div className={`NavCont ${theme}`}>
@@ -22,19 +38,57 @@ const Navbar = () => {
           <img alt="icon" src={BurgerMenu} />
         </div>
       </div>
+
       <ul className={`UlCont ${isOpen ? "open" : ""}`}>
+        {isOpen && (
+          <img
+            className="x"
+            alt="close icon"
+            src={theme === "dark" ? xLight : x}
+            onClick={closeMenu}
+          />
+        )}
         <li>
+          <img
+            src={theme === "dark" ? homeIconLight : homeIcon}
+            alt="homeIcon"
+          />
           <Link to="/">{TEXTS[language].home}</Link>{" "}
         </li>
+        <hr />
         <li>
+          <img
+            src={theme === "dark" ? sercvIconLight : sercvIcon}
+            alt="sersvIcon"
+          />
           <Link to="/Service">{TEXTS[language].ourService}</Link>{" "}
         </li>
+        <hr />
         <li>
+          <img
+            src={theme === "dark" ? priceIconLight : priceIcon}
+            alt="priceIcon"
+          />
+          <Link to="/Prices">{TEXTS[language].prices}</Link>{" "}
+        </li>
+        <hr />
+        <li>
+          <img
+            src={theme === "dark" ? aboutIconLight : aboutIcon}
+            alt="aboutIcon"
+          />
           <Link to="/About">{TEXTS[language].about}</Link>{" "}
         </li>
+        <hr />
         <li>
+          <img
+            src={theme === "dark" ? contactIconLight : contactIcon}
+            alt="contactIcon"
+          />
           <Link to="/Contact">{TEXTS[language].contact}</Link>{" "}
         </li>
+        <hr />
+        <img className="mobileImageNavbar" alt="logo" src={mobImage} />
       </ul>
     </div>
   );
