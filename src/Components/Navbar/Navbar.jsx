2dfbb1f3b7f3b54f93e34a6f32be10a70c2d48f9
anano303 <124179773+anano303@users.noreply.github.com>
2024-09-15 20:20:13 +0400

    burgermenu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,33 +4,40 @@ import { LanguageContext } from "../../Hooks/LanguageContext";
 import { TEXTS } from "../../Hooks/Languages";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import BurgerMenu from "./Icons/burger menu icon.png";
 
 const Navbar = () => {
-    const { theme, toggleTheme } = useContext(ThemeContext);
-    const { language, setLanguage } = useContext(LanguageContext);
-    const [isOpen, setIsOpen] = useState(false);
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const { language, setLanguage } = useContext(LanguageContext);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <div className={`NavCont ${theme}`}>
-            <div className="hamburger-menu" onClick={toggleMenu}>
-                <div className={`burger ${isOpen ? 'open' : ''}`}>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
-                </div>
-            </div>
-            <ul className={`UlCont ${isOpen ? "open" : ""}`}>
-                <li><Link to="/">{TEXTS[language].home}</Link> </li>
-                <li><Link to="/Service">{TEXTS[language].service}</Link> </li>
-                <li><Link to="/About">{TEXTS[language].about}</Link> </li>
-                <li><Link to="/Contact">{TEXTS[language].contact}</Link> </li>
-            </ul>
+  return (
+    <div className={`NavCont ${theme}`}>
+      <div className="hamburger-menu" onClick={toggleMenu}>
+        <div className={`burger ${isOpen ? "open" : ""}`}>
+          <img alt="icon" src={BurgerMenu} />
         </div>
-    );
+      </div>
+      <ul className={`UlCont ${isOpen ? "open" : ""}`}>
+        <li>
+          <Link to="/">{TEXTS[language].home}</Link>{" "}
+        </li>
+        <li>
+          <Link to="/Service">{TEXTS[language].ourService}</Link>{" "}
+        </li>
+        <li>
+          <Link to="/About">{TEXTS[language].about}</Link>{" "}
+        </li>
+        <li>
+          <Link to="/Contact">{TEXTS[language].contact}</Link>{" "}
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
