1b6a45ccbffaae3ced92cc082040e3482f24bc95
anano303 <124179773+anano303@users.noreply.github.com>
2024-09-24 16:21:03 +0400

    bags

--- src/Pages/Prices/Prices.jsx
+++ src/Pages/Prices/Prices.jsx
@@ -1,5 +1,14 @@
+import headPicture from "./head picture.png";
+
 const Prices = () => {
-  return <div className="prices">ფასები</div>;
+  return (
+    <div className="prices">
+      <div className="servSect1 contactSect1 priceSect1">
+        <img alt="contactImage" src={headPicture} className="image" />{" "}
+      </div>
+      ფასები
+    </div>
+  );
 };
 
 export default Prices;
