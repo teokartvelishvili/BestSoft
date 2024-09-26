aa3e844c2fc5877fe2f97bbfcfd736a1c4b3ca25
anano303 <124179773+anano303@users.noreply.github.com>
2024-09-26 22:09:51 +0400

    add service page

--- src/Pages/Service/Service.jsx
+++ src/Pages/Service/Service.jsx
@@ -5,15 +5,16 @@ import { TEXTS } from "../../Hooks/Languages";
 import "./Service.css";
 import darkImage1 from "./Images/serviceImageDark.png";
 import lightImage1 from "./Images/serviceImage.png";
-import Image1 from "./Images/image 1.png";
-import Image2 from "./Images/image 2.png";
-import Image3 from "./Images/image 3.png";
-import Image4 from "./Images/image 4.png";
-import Buttons from "../../Components/Button/Buttons";
-import lineDark from "./Images/Line dark.png";
-import lineLight from "./Images/Line light.png";
+// import Image1 from "./Images/image 1.png";
+// import Image2 from "./Images/image 2.png";
+// import Image3 from "./Images/image 3.png";
+// import Image4 from "./Images/image 4.png";
+// import Buttons from "../../Components/Button/Buttons";
+// import lineDark from "./Images/Line dark.png";
+// import lineLight from "./Images/Line light.png";
+// import LinkButton from "../../Components/LinkButton/LinkButton";
 import LinkButton from "../../Components/LinkButton/LinkButton";
-
+import { Link } from "react-router-dom";
 const Service = () => {
   const { theme } = useContext(ThemeContext);
   const { language } = useContext(LanguageContext);
@@ -27,222 +28,208 @@ const Service = () => {
           className="image"
         />
       </div>
-      <div className="servSect2">
-        <div className="servText">
-          <h1>{TEXTS[language].webSites}</h1>
-          <p>{TEXTS[language].webSitesDescription}</p>
+      <div className="serviceCardsGrid">
+        <div className="serviceCard FR">
+          <h2>FRONT - END დეველოპმენტი</h2>
+          <h3>ვებ-გვერდისა და აპლიკაციების წინა მხარის განვითარება</h3>
+          <ul>
+            <li>
+              ვებსაიტების შექმნა HTML, CSS და JavaScript/Typescript-ის
+              გამოყენებით
+            </li>
+            <li>
+              სივი/პორტფოლიოს საიტები, ლენდინგ ფეიჯები, ბლოგები და ბიზნესის
+              ვებსაიტები
+            </li>
+            <li>
+              რეაქტიული (Responsive) დიზაინი, რომელიც ყველა მოწყობილობაზე კარგად
+              გამოიყურება
+            </li>
+            <li>
+              ვებ-ანიმაციები და ინტერაქტიული ელემენტები, რაც საიტის ვიზუალურ
+              მხარეს გაამრავალფეროვნებს
+            </li>
+            <li>
+              ტექნოლოგიები და ხელსაწყოები (React, Angular, Next.js) რთული
+              ვებსაიტების ან აპლიკაციების შესაქმნელად
+            </li>
+            <li>
+              კროს-ბრაუზერული თავსებადობა, რათა საიტი ყველგან კარგად მუშაობდეს
+            </li>
+          </ul>
+
+          <h3>UX/UI ინტეგრაცია</h3>
+          <ul>
+            <li>
+              UX/UI დიზაინის იმპლემენტაცია და ოპტიმიზაცია, რომ საიტი იყოს
+              მოსახერხებელი და ვიზუალურად მიმზიდველი
+            </li>
+            <li>
+              ქსელის ოპტიმიზაცია და გვერდების ჩატვირთვის სისწრაფის გაუმჯობესება
+            </li>
+            <li>
+              ინტერაქტიული კომპონენტების დამატება, როგორიცაა ნავიგაცია, ფორმები,
+              ღილაკები და სხვა ელემენტები
+            </li>
+          </ul>
+
+          <h3>ვებსაიტების ოპტიმიზაცია</h3>
+          <ul>
+            <li>
+              SEO ოპტიმიზაცია, რათა საიტი საძიებო სისტემებში მაღალ პოზიციაზე
+              იყოს
+            </li>
+            <li>
+              პროგრესული ვებ აპლიკაციების (PWA) განვითარება, რომ საიტი იმუშაოს
+              როგორც ნატურალური აპლიკაცია
+            </li>
+            <li>
+              ვებსაიტის წვდომის გაუმჯობესება და შშმ პირებისთვის ხელმისაწვდომი
+              დიზაინი
+            </li>
+          </ul>
+
+          <h3>მუდმივი მხარდაჭერა და განახლება</h3>
+          <ul>
+            <li>ვებსაიტის ტექნიკური მხარდაჭერა და რეგულარული განახლებები</li>
+            <li>
+              ახალი ფუნქციების დამატება, როგორიცაა ფორუმი, გალერეა, სოციალური
+              მედიის ინტეგრაცია და სხვა
+            </li>
+            <li>საიტის რეგულარული ტესტირება და გაუმჯობესება</li>
+          </ul>
         </div>
-        <div  className="servBox servBox1">
-          <div id="frontEnd" className="overview-container">
-            {/* <div className="overview-section">
-              <h3 className="overview-title">{TEXTS[language].overview}</h3>
-              <ul>
-                <li>{TEXTS[language].overview1}</li>
-                <li>{TEXTS[language].overview2}</li>
-              </ul>
-            </div> */}
-            <h1>{TEXTS[language].frontEnd}</h1>
-            <p>{TEXTS[language].frontEndDescription}</p>
 
+        <div className="serviceCard BK">
+          <h2>BACK - END დეველოპმენტი</h2>
+          <h3>სერვერის მხარის განვითარება და მენეჯმენტი</h3>
+          <ul>
+            <li>
+              ვებ აპლიკაციების სერვერის მხარის განვითარება, როგორიცაა
+              რეგისტრაცია/ავტორიზაცია, მომხმარებლის პროფილები
+            </li>
+            <li>ფორუმი, ბლოგი, კომუნიკაციის სისტემები და ჩეთები</li>
+            <li>
+              გადახდის სისტემები, სადაც მომხმარებელს შეუძლია პროდუქტების ყიდვა
+            </li>
+            <li>
+              სერვერის არქიტექტურის შექმნა და API-ების შემუშავება, რომელიც
+              კომუნიკაციას ახდენს წინა მხარესთან
+            </li>
+            <li>
+              მონაცემთა ბაზის დიზაინი და მართვა (SQL და NoSQL ტექნოლოგიები)
+            </li>
+            <li>
+              უსაფრთხოების მექანიზმების იმპლემენტაცია, რათა დაცული იყოს
+              მომხმარებლის მონაცემები
+            </li>
+          </ul>
 
-            <div className="overview-section">
-              <h3>{TEXTS[language].technologies}</h3>
-              <ul>
-                <li>{TEXTS[language].technologies1}</li>
-                <li>{TEXTS[language].technologies2}</li>
-              </ul>
-            </div>
+          <h3>ინტეგრაცია და მასშტაბურობა</h3>
+          <ul>
+            <li>
+              მონაცემთა მასშტაბირებადი სისტემების შექმნა, რათა დიდი რაოდენობის
+              მომხმარებელი ერთდროულად ემსახუროს
+            </li>
+            <li>
+              ქლაუდ სერვისების ინტეგრაცია (AWS, Google Cloud, Azure) საიტის
+              სანდოობისა და სისწრაფის უზრუნველსაყოფად
+            </li>
+            <li>
+              რეალურ დროში მონაცემების მართვა (WebSockets, ჩატები,
+              ნოტიფიკაციები)
+            </li>
+          </ul>
 
-            <div className="overview-section">
-              <h3>{TEXTS[language].keyFeatures}</h3>
-              <ul>
-                <li>{TEXTS[language].keyFeatures1}</li>
-                <li>{TEXTS[language].keyFeatures2}</li>
-                <li>{TEXTS[language].keyFeatures3}</li>
-                <li>{TEXTS[language].keyFeatures4}</li>
-              </ul>
-            </div>
+          <h3>უსაფრთხოება და შესრულება</h3>
+          <ul>
+            <li>
+              სერვერის უსაფრთხოების გაუმჯობესება, მონაცემების დაცვა და
+              ქურდობისგან დაცვა
+            </li>
+            <li>სერვერის პერფორმანსის ოპტიმიზაცია და მუდმივი მონიტორინგი</li>
+            <li>ხარვეზების აღმოფხვრა და სისტემის გაუმჯობესება</li>
+          </ul>
 
-            <div className="overview-section">
-              <h3>{TEXTS[language].process}</h3>
-              <ul>
-                <li>{TEXTS[language].process1}</li>
-                <li>{TEXTS[language].process2}</li>
-                <li>{TEXTS[language].process3}</li>
-                <li>{TEXTS[language].process4}</li>
-                <li>{TEXTS[language].process5}</li>
-              </ul>
-            </div>
-          </div>
-          <img className="boxImage" src={Image1} alt="img" />
+          <h3>მუდმივი მხარდაჭერა და ტექნიკური მომსახურება</h3>
+          <ul>
+            <li>სერვერის და მონაცემთა ბაზის მუდმივი ტექნიკური მხარდაჭერა</li>
+            <li>ახალი ფუნქციების დამატება და სისტემის განახლება</li>
+            <li>სერვერის რეგულარული მონიტორინგი და გაუმჯობესება</li>
+          </ul>
         </div>
-        {/* <Buttons />
-        <img
-          className="lineServ"
-          src={theme === "dark" ? lineDark : lineLight}
-          alt="line"
-        /> */}
-      </div>
-
-      <div className="servSect2 servSect3">
-        {/* <div className="servText">
-          <h1>{TEXTS[language].backEnd}</h1>
-          <p>{TEXTS[language].backEndDescription}</p>
-        </div> */}
-        <div className="servBox servBox2">
-          <img className="boxImage" src={Image2} alt="img" />
-          <div id="backEnd" className="overview-container">
-            {/* <div className="overview-section">
-              <h3 className="overview-title">{TEXTS[language].overview}</h3>
-              <ul>
-                <li>{TEXTS[language].overview1}</li>
-                <li>{TEXTS[language].overview2}</li>
-              </ul>
-            </div> */}
-            <h1>{TEXTS[language].backEnd}</h1>
-            <p>{TEXTS[language].backEndDescription}</p>
-
-            <div className="overview-section">
-              <h3>{TEXTS[language].technologiesUsed}</h3>
-              <ul>
-                <li>{TEXTS[language].technologiesUsed1}</li>
-                <li>{TEXTS[language].technologiesUsed2}</li>
-                <li>{TEXTS[language].technologiesUsed3}</li>
-              </ul>
-            </div>
-
-            <div className="overview-section">
-              <h3>{TEXTS[language].keyFeatures}</h3>
-              <ul>
-                <li>{TEXTS[language].keyFeatures1}</li>
-                <li>{TEXTS[language].keyFeatures2}</li>
-                <li>{TEXTS[language].keyFeatures3}</li>
-                <li>{TEXTS[language].keyFeatures4}</li>
-              </ul>
-            </div>
-
-            <div className="overview-section">
-              <h3>{TEXTS[language].process}</h3>
-              <ul>
-                <li>{TEXTS[language].process1}</li>
-                <li>{TEXTS[language].process2}</li>
-                <li>{TEXTS[language].process3}</li>
-                <li>{TEXTS[language].process4}</li>
-                <li>{TEXTS[language].process5}</li>
-              </ul>
-            </div>
-          </div>
+        <div className="serviceCard GD">
+          <h2>გრაფიკული დიზაინი</h2>
+          <h3>ლოგოსა და ბრენდის იდენტობის დიზაინი</h3>
+          <ul>
+            <li>ლოგოს შექმნა</li>
+            <li>სრული ბრენდ-იდენტობის პაკეტი</li>
+            <li>სავიზიტო ბარათები</li>
+            <li>
+              სოციალური მედიის ბრენდინგი (პროფილის სურათები, ბანერები და პოსტის
+              შაბლონები)
+            </li>
+          </ul>
+          <h3>ბეჭდვითი დიზაინი</h3>
+          <ul>
+            <li>ბროშურები, ფლაერები და პოსტერი</li>
+            <li>პროდუქტის შეფუთვის დიზაინი</li>
+            <li>ღონისძიების მასალები: მოსაწვევები, პროგრამები, ბანერები</li>
+            <li>
+              პროდუქციის დიზაინი (ფეხსაცმელი, მაისურები, სუვენირები და სხვა)
+            </li>
+          </ul>
+          <h3>მარკეტინგის მასალები</h3>
+          <ul>
+            <li>გაყიდვების პრეზენტაციები და პიჩ-დეკები</li>
+            <li>კორპორაციული ბროშურები, კატალოგები</li>
+            <li>სარეკლამო პოსტერები და ბანერები</li>
+          </ul>
+          <h3>ილუსტრაციები</h3>
+          <ul>
+            <li>
+              ილუსტრაციები რედაქტორისთვის, ბრენდინგისთვის ან პროდუქციისთვის
+            </li>
+            <li>პერსონალური ილუსტრაციები</li>
+            <li>წიგნის ილუსტრაციები</li>
+          </ul>
+          <Link to="/Calculation">
+            <button className="priceButton">კალკულაცია</button>
+          </Link>{" "}
         </div>
-        <Buttons />
-        <img
-          className="lineServ"
-          src={theme === "dark" ? lineDark : lineLight}
-          alt="line"
-        />
-      </div>
+        <div className="serviceCard UX">
+          <h2>UX/UI დიზაინი</h2>
+          <h3>UX დიზაინი (მომხმარებლის გამოცდილება)</h3>
+          <ul>
+            <li>მომხმარებლის კვლევა</li>
+            <li>ინფორმაციის არქიტექტურა</li>
+            <li>ვაირფრემები და პროტოტიპები</li>
+            <li>მომხმარებლის ტესტირება და უკუკავშირი</li>
+            <li>ინტერაქციული დიზაინი</li>
+          </ul>
 
-      <div id="graphic" className="servSect2">
-        <div className="servText">
-          <h1>{TEXTS[language].graphicDesign}</h1>
-          <p>{TEXTS[language].graphicDesignDescription}</p>
+          <h3>UI დიზაინი (მომხმარებლის ინტერფეისი)</h3>
+          <ul>
+            <li>ვიზუალური დიზაინი</li>
+            <li>რესფონსიული დიზაინი</li>
+            <li>დიზაინის სისტემები და სტილები</li>
+            <li>მაღალი ხარისხის მოქაფები</li>
+          </ul>
+
+          <h3>ვებსაიტისა და აპლიკაციის დიზაინი</h3>
+          <ul>
+            <li>ლენდინგ გვერდები</li>
+            <li>ეკომერსის გვერდები</li>
+            <li>მობილური აპლიკაციის ინტერფეისი</li>
+          </ul>
+
+          <h3>განახლება და ოპტიმიზაცია</h3>
+          <ul>
+            <li>UX აუდიტები</li>
+            <li>მომხმარებლისთვის მორგებული განახლებები</li>
+            <li>A/B ტესტირება</li>
+          </ul>
         </div>
-        <div className="servBox servBox1">
-          <div className="overview-container">
-            {/* <div className="overview-section">
-              <h3 className="overview-title">{TEXTS[language].overview}</h3>
-              <ul>
-                <li>{TEXTS[language].overview1}</li>
-                <li>{TEXTS[language].overview2}</li>
-              </ul>
-            </div> */}
-
-            <div className="overview-section">
-              <h3>{TEXTS[language].servicesOffered}</h3>
-              <ul>
-                <li>{TEXTS[language].servicesOffered1}</li>
-                <li>{TEXTS[language].servicesOffered2}</li>
-                <li>{TEXTS[language].servicesOffered3}</li>
-                <li>{TEXTS[language].servicesOffered4}</li>
-              </ul>
-            </div>
-
-            <div className="overview-section">
-              <h3>{TEXTS[language].keyFeatures}</h3>
-              <ul>
-                <li>{TEXTS[language].keyFeatures1}</li>
-                <li>{TEXTS[language].keyFeatures2}</li>
-                <li>{TEXTS[language].keyFeatures3}</li>
-                <li>{TEXTS[language].keyFeatures4}</li>
-              </ul>
-            </div>
-
-            <div className="overview-section">
-              <h3>{TEXTS[language].process}</h3>
-              <ul>
-                <li>{TEXTS[language].process1}</li>
-                <li>{TEXTS[language].process2}</li>
-                <li>{TEXTS[language].process3}</li>
-                <li>{TEXTS[language].process4}</li>
-                <li>{TEXTS[language].process5}</li>
-              </ul>
-            </div>
-          </div>
-          <img className="boxImage" src={Image3} alt="img" />
-        </div>
-        <Buttons />
-        <img
-          className="lineServ"
-          src={theme === "dark" ? lineDark : lineLight}
-          alt="line"
-        />
-      </div>
-
-      <div id="ux" className="servSect2">
-        <div className="servText">
-          <h1>{TEXTS[language].uxUiDesign}</h1>
-          <p>{TEXTS[language].uxUiDesignDescription}</p>
-        </div>
-        <div className="servBox servBox1">
-          <div className="overview-container">
-            {/* <div className="overview-section">
-              <h3 className="overview-title">{TEXTS[language].overview}</h3>
-              <ul>
-                <li>{TEXTS[language].overview1}</li>
-                <li>{TEXTS[language].overview2}</li>
-              </ul>
-            </div> */}
-
-            <div className="overview-section">
-              <h3>{TEXTS[language].toolsUsed}</h3>
-              <ul>
-                <li>{TEXTS[language].toolsUsed1}</li>
-                <li>{TEXTS[language].toolsUsed2}</li>
-              </ul>
-            </div>
-
-            <div className="overview-section">
-              <h3>{TEXTS[language].keyFeatures}</h3>
-              <ul>
-                <li>{TEXTS[language].keyFeatures1}</li>
-                <li>{TEXTS[language].keyFeatures2}</li>
-                <li>{TEXTS[language].keyFeatures3}</li>
-              </ul>
-            </div>
-
-            <div className="overview-section">
-              <h3>{TEXTS[language].process}</h3>
-              <ul>
-                <li>{TEXTS[language].process1}</li>
-                <li>{TEXTS[language].process2}</li>
-                <li>{TEXTS[language].process3}</li>
-                <li>{TEXTS[language].process4}</li>
-                <li>{TEXTS[language].process5}</li>
-              </ul>
-            </div>
-          </div>
-          <img className="boxImage" src={Image4} alt="img" />
-        </div>
-        <Buttons />
       </div>
     </div>
   );
