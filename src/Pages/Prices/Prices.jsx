8d61cd28d76fedcf52029e727f0e7466cf8914c3
teokartvelishvili <qartvelishviliteona@gmail.com>
2024-09-27 01:35:23 -0800

    ..

--- src/Pages/Prices/Prices.jsx
+++ src/Pages/Prices/Prices.jsx
@@ -1,18 +1,33 @@
+import React, { useState, useEffect } from "react";
 import headPicture from "./head picture.png";
 import "./Prices.css";
 import CircleEffect from "../../Components/CircleEffect/CircleEffect";
 import CircleSquare from "../../Components/CircleSquare/CircleSquare";
 import CalculationPage from "../Calculator/Calculation";
 
-
 const Prices = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="prices">
-      <CircleEffect />
-      <div className="servSect1 contactSect1priceSect1">
-        {/* <img alt="contactImage" src={headPicture} className="image" />{" "} */}
-        <CircleSquare/>
-        <CalculationPage/>
+      {!isMobile && <CircleEffect />}
+
+      <div className="servSect1 contactSect1 priceSect1">
+        {isMobile ? (
+          <img alt="contactImage" src={headPicture} className="image" />
+        ) : null}
+        {!isMobile && <CircleSquare />}
+        <CalculationPage />
       </div>
     </div>
   );
